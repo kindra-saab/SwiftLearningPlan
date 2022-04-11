ab77f430d1640817ba424e607e3b07756a0e25b7
kindra-saab <jatin0687.cse19@chitkara.edu.in>
2022-04-11 12:29:45 +0530

    updates

---
+++ swiftDemo/swiftDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftDemo
+//
+//  Created by Jatin Kindra on 11/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
